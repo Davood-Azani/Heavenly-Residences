83c47cb4af9f21de12aa8a5b5bd63f01b3dd03bc
f00000b <f00000b@gmail.com>
2023-04-27 00:03:36 +0330

    init

---
+++ Heaven Resorts_Server/Model/BlazorAmenities.cs
@@ -0,0 +1,9 @@
+﻿namespace Heaven_Resorts_Server.Model
+{
+    public class BlazorAmenities
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
