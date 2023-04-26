3173176cfb6bea6f6a5a3f12ce4b37c3079dc5af
f00000b <f00000b@gmail.com>
2023-04-27 00:48:37 +0330

    adding dbc6ntext and CnStringSetting and StarupSetting with First Migration

---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)   
+        {
+            
+        }
+    }
+}
--- Heaven Resorts_Server/Startup.cs
+++ Heaven Resorts_Server/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Heaven_Resorts_Server
 {
@@ -26,6 +28,8 @@ namespace Heaven_Resorts_Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(option =>
+                option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
